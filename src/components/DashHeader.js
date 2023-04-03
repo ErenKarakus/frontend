39812b044bd0eff94d3c112c7e1ad06a58d083cf
ErenKarakus <73061472+ErenKarakus@users.noreply.github.com>
2023-04-03 19:33:31 +0100

    roles & permissions

--- src/components/DashHeader.js
+++ src/components/DashHeader.js
@@ -1,15 +1,22 @@
 import { useEffect } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faRightFromBracket } from "@fortawesome/free-solid-svg-icons"
+import { 
+    faUserGear,
+    faUserPlus,
+    faRightFromBracket 
+} from "@fortawesome/free-solid-svg-icons"
 import { useNavigate, Link, useLocation } from 'react-router-dom'
 
 import { useSendLogoutMutation } from '../features/auth/authApiSlice'
+
+import useAuth from '../hooks/useAuth'
 
 const DASH_REGEX = /^\/dash(\/)?$/
 const NOTES_REGEX = /^\/dash\/notes(\/)?$/
 const USERS_REGEX = /^\/dash\/users(\/)?$/
 
 const DashHeader = () => {
+    const { isManager, isAdmin } = useAuth()
 
     const navigate = useNavigate()
     const { pathname } = useLocation()
@@ -25,13 +32,40 @@ const DashHeader = () => {
         if (isSuccess) navigate('/')
     }, [isSuccess, navigate])
 
-    if (isLoading) return <p>Logging Out...</p>
-
-    if (isError) return <p>Error: {error.data?.message}</p>
+    const onNewUserClicked = () => navigate('/dash/users/new')
+    const onUsersClicked = () => navigate('/dash/users')
 
     let dashClass = null
     if (!DASH_REGEX.test(pathname) && !NOTES_REGEX.test(pathname) && !USERS_REGEX.test(pathname)) {
         dashClass = "dash-header__container--small"
+    }
+
+    let newUserButton = null
+    if (USERS_REGEX.test(pathname)) {
+        newUserButton = (
+            <button
+                className="icon-button"
+                title="New User"
+                onClick={onNewUserClicked}
+            >
+                <FontAwesomeIcon icon={faUserPlus} />
+            </button>
+        )
+    }
+
+    let userButton = null
+    if (isManager || isAdmin) {
+        if (!USERS_REGEX.test(pathname) && pathname.includes('/dash')) {
+            userButton = (
+                <button
+                    className="icon-button"
+                    title="Users"
+                    onClick={onUsersClicked}
+                >
+                    <FontAwesomeIcon icon={faUserGear} />
+                </button>
+            )
+        }
     }
 
     const logoutButton = (
@@ -44,18 +78,36 @@ const DashHeader = () => {
         </button>
     )
 
+    const errClass = isError ? "errmsg" : "offscreen"
+
+    let buttonContent
+    if (isLoading) {
+        buttonContent = <p>Logging Out...</p>
+    } else {
+        buttonContent = (
+            <>  
+                {newUserButton}
+                {userButton}
+                {logoutButton}
+            </>
+        )
+    }
+
     const content = (
-        <header className="dash-header">
-            <div className={`dash-header__container ${dashClass}`}>
-                <Link to="/dash">
-                    <h1 className="dash-header__title">techNotes</h1>
-                </Link>
-                <nav className="dash-header__nav">
-                    {/* add more buttons later */}
-                    {logoutButton}
-                </nav>
-            </div>
+        <>
+            <p className={errClass}>{error?.data?.message}</p>
+            
+            <header className="dash-header">
+                <div className={`dash-header__container ${dashClass}`}>
+                    <Link to="/dash">
+                        <h1 className="dash-header__title">techNotes</h1>
+                    </Link>
+                    <nav className="dash-header__nav">
+                        {buttonContent}
+                    </nav>
+                </div>
         </header>
+        </>
     )
 
     return content
--- src/features/auth/Welcome.js
+++ src/features/auth/Welcome.js
@@ -1,6 +1,9 @@
 import { Link } from 'react-router-dom'
+import useAuth from '../../hooks/useAuth'
 
 const Welcome = () => {
+
+    const { username, isManager, isAdmin } = useAuth()
 
     const date = new Date()
     const today = new Intl.DateTimeFormat('en-GB', { dateStyle: 'full', timeStyle: 'long' }).format(date)
@@ -10,10 +13,11 @@ const Welcome = () => {
 
             <p>{today}</p>
 
-            <h1>Welcome!</h1>
+            <h1>Welcome {username}!</h1>
+            
+            {(isManager || isAdmin) && <p><Link to="/dash/users">View User Settings</Link></p>}
 
-            <p><Link to="/dash/users">View User Settings</Link></p>
-            <p><Link to="/dash/users/new">Add New User</Link></p>
+            {(isManager || isAdmin) && <p><Link to="/dash/users/new">Add New User</Link></p>}
 
         </section>
     )
